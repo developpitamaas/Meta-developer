03d75a6ba4dfadd22e4915e5ba3d78d944f146d9
thevaibhavrr <vaibhavrathorema@gmail.com>
2025-06-09 11:15:53 +0530

    added empty line

--- routes/facebook/stroy.js
+++ routes/facebook/stroy.js
@@ -4,9 +4,10 @@ const Data = require("../../controller/Story/story");
 
 
 
+
 story.route("/post-story").post(Data.postStory);
 story.route("/get-story").get(Data.getStory);
 story.route("/delete-story/:id").delete(Data.deleteStory);
-
+   
 
 module.exports = story
